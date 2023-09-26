b795e33d41a85cdda0e8e4b8df4ca6a3404f04ef
Ibukunoluwa Folajimi <folajimii@wit.edu>
2023-09-26 11:50:51 -0400

    initial commit

---
+++ favoritedayoftheweek/src/favoritedayoftheweek/Favoritedayoftheweek.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package favoritedayoftheweek;
+
+/**
+ * 
+ */
+public class Favoritedayoftheweek {
+
+	/**
+	 * 
+	 */
+	public Favoritedayoftheweek() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
