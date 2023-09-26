9a9b4fc5ef58c0709614e8aaa39b9d78789c7771
folajimiiatwit <144471241+folajimiiatwit@users.noreply.github.com>
2023-09-26 12:09:30 -0400

    Add files via upload

---
+++ G05/favoritedayoftheweek/src/favoritedayoftheweek/Favoritedayoftheweek.java
@@ -0,0 +1,25 @@
+package favoritedayoftheweek;
+import java.util.Scanner;
+/**
+ * 
+ */
+public class Favoritedayoftheweek {
+
+	/**
+	 * 
+	 */
+	public Favoritedayoftheweek() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		System.out.print("What is your favorite day of the week?" );
+		String day = console.next(); 
+
+	}
+
+}
